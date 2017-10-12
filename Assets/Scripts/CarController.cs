dffd6747df1f52c2fca97bb946e237f249785527
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-12 00:58:40 -0300

    feat: Fisica basica do carro criada

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public float speed=300;
+	public WheelJoint2D backWheel;
+	public WheelJoint2D frontWheel;
+	
+	// Update is called once per frame
+	void Update () {
+		JointMotor2D backMotor = backWheel.motor;
+		JointMotor2D frontMotor = frontWheel.motor;
+
+		backMotor.motorSpeed=speed * -Input.GetAxis ("Vertical");
+		frontMotor.motorSpeed=speed * -Input.GetAxis ("Vertical");
+
+		backWheel.motor = backMotor;
+		frontWheel.motor = frontMotor;
+	}
+}
