f1607adf2462852258ccbab98a0a6e5686989f35
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-12 02:25:14 -0300

    feat: Checkpoint de vitoria implementado

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	bool hasWinner;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (!hasWinner) {
+			if (collider.transform.parent.name == "CarPlayer1") {
+				LevelController.instance.SetWinner (1);
+				hasWinner = true;
+			} else if (collider.transform.parent.name == "CarPlayer2") {
+				LevelController.instance.SetWinner (2);
+				hasWinner = true;
+			}
+		}
+	}
+}
