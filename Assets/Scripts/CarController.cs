01c2d5fab6914851ea5e22393990443b4028d89e
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-12 01:52:10 -0300

    feat: Controle multiplayer configurado

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour {
+
+	public int controllerNumber;
 	public float speed=300;
 	public float rotateSpeed=0.5f;
 	public WheelJoint2D backWheel;
@@ -17,12 +19,12 @@ public class CarController : MonoBehaviour {
 		JointMotor2D backMotor = backWheel.motor;
 		JointMotor2D frontMotor = frontWheel.motor;
 
-		backMotor.motorSpeed=speed * -Input.GetAxis ("Vertical");
-		frontMotor.motorSpeed=speed * -Input.GetAxis ("Vertical");
+		backMotor.motorSpeed=speed * -Input.GetAxis ("Vertical"+controllerNumber);
+		frontMotor.motorSpeed=speed * -Input.GetAxis ("Vertical"+controllerNumber);
 
 		backWheel.motor = backMotor;
 		frontWheel.motor = frontMotor;
 
-		myRigdbody2D.AddTorque (backMotor.motorSpeed * rotateSpeed * Input.GetAxis ("Horizontal"));
+		myRigdbody2D.AddTorque (backMotor.motorSpeed * rotateSpeed * Input.GetAxis ("Horizontal"+controllerNumber));
 	}
 }
