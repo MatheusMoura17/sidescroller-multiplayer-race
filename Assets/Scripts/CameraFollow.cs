3d4bc6e09d08af4217abad087edc362bf4f465ba
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-12 02:00:42 -0300

    feat: Script de seguir player criado

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float speed=10;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = transform.position;
+		targetPosition.x = target.position.x;
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, speed * Time.deltaTime);
+	}
+}
