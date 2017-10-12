1be463bd5e1210a09465ddf055fc122b193ae63e
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-12 02:24:55 -0300

    feat: Level controller implementado

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+	public static LevelController instance;
+
+	public GameObject winnerPanel;
+	public Text winnerText;
+
+	void Awake(){
+		instance = this;
+	}
+
+	public void Retry(){
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void SetWinner(int winner){
+		winnerPanel.SetActive(true);
+		winnerText.text="Player "+winner+" venceu!";
+	}
+}
